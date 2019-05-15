b36d4ff64688365c1a8acb6f8b0b84c895e943ff
ManicDrake <dragonzorb@gmail.com>
2019-05-14 21:13:53 -0700

    Add Varient Mover

---
+++ Deathknight/Assets/Scripts/noidMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class noidMover : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 targetVector;
+    NavMeshAgent _navmeshAgent;
+
+    void Start()
+    {
+        _navmeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navmeshAgent == null) {
+            Debug.LogError("nav mesh agent Componenet not attached to " + gameObject.name);
+        }
+    }
+
+    void Update() {
+        // targetVector = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        targetVector = gameObject.transform.position;
+        targetVector.x += Input.GetAxis("Horizontal");
+        targetVector.z += Input.GetAxis("Vertical");
+        SetDestination();
+    }
+
+    private void SetDestination() {
+        if(targetVector != null) {
+            _navmeshAgent.SetDestination(targetVector);
+        }
+    }
+}
