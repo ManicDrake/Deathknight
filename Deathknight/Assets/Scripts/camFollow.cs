955843a0cf453c1ff5fec434a91984d124ae622d
ManicDrake <dragonzorb@gmail.com>
2019-05-14 21:13:27 -0700

    update Camera

---
+++ Deathknight/Assets/Scripts/camFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollow : MonoBehaviour {
+
+    public GameObject subject; 
+    private Vector3 offset;         
+    void Start () 
+    {
+        offset = transform.position - subject.transform.position;
+    }
+    void LateUpdate () 
+    {
+        transform.position = subject.transform.position + offset;
+    }
+}
