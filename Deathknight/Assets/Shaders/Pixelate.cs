acbf70ba19818789de881d4436d2ac94fdaaf43d
ManicDrake <dragonzorb@gmail.com>
2019-05-20 15:40:26 -0700

    Moving things around add some Prefab assets from store as Temp

---
+++ Deathknight/Assets/Shaders/Pixelate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Pixelate : MonoBehaviour
+{
+    public Material effectMat;
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) 
+    {
+        Graphics.Blit(src, dest, effectMat);
+    }
+}
