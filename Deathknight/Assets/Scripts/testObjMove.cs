60044693b65f650cc6beea59c9f9d07f70c3bb95
ManicDrake <dragonzorb@gmail.com>
2019-06-04 14:43:43 -0700

    add test Platform make script to move platform

---
+++ Deathknight/Assets/Scripts/testObjMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testObjMove : MonoBehaviour
+{
+    private Rigidbody rigBody;
+    public float moveSpeed = 1.0f;
+    public float xMoveSize = 3.0f; public float yMoveSize = 3.0f;
+    private Vector3 startLocation;
+    void Start()
+    {
+        startLocation = transform.position;
+        rigBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position = new Vector3(Mathf.Sin(Time.time * moveSpeed) * xMoveSize, Mathf.Cos(Time.time * moveSpeed) * yMoveSize, 0.0f) + startLocation;
+        
+    }
+
+    private void FixedUpdate() {
+        rigBody.MovePosition(new Vector3(Mathf.Sin(Time.time * moveSpeed) * xMoveSize, Mathf.Cos(Time.time * moveSpeed) * yMoveSize, 0.0f) + startLocation);
+    }
+}
