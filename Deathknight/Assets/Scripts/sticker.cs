c11c76696f71b646add629e2a734e5ee0cb72fb8
ManicDrake <dragonzorb@gmail.com>
2019-06-06 16:54:07 -0700

    Move Scripts

---
+++ Deathknight/Assets/Scripts/sticker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//private Transform attachsadagas;
+public class sticker : MonoBehaviour
+{
+    // void Start()
+    // {
+        
+    // }
+    private void OnCollisionEnter(Collision other) {
+        if(other.transform.GetComponent<Rigidbody>()) {
+            transform.SetParent(other.transform);
+        }
+    }
+    private void OnCollisionExit(Collision other) {
+        transform.parent = null;
+    }
+    public bool Approximately(Vector3 me, Vector3 other, float percentage)
+        {
+            var dx = Mathf.Abs(me.x) - other.x;
+            if (dx > other.x * percentage)
+                return false;
+ 
+            var dy = Mathf.Abs(me.y) - other.y;
+            if (dy > other.y * percentage)
+                return false;
+
+            var dz = Mathf.Abs(me.z) - other.z;
+
+            return dz > me.z * percentage;
+        }
+}
